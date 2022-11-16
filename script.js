2381e7e5e3c33d0220e0fa851b566c765b6d4b21
Melanie-J-Baker <baker_mel@hotmail.com>
2022-11-16 13:35:59 +0000

    Create function to create board array

---
+++ script.js
@@ -0,0 +1,28 @@
+// factory function to create players
+
+//gameBoard object - module
+const gameBoard = (() => {
+
+    //Create board array
+    let board = [];
+    for (i = 0; i < 9; i++) {
+        board.push("");
+    }
+    //Create square div for each array item
+    //Add event listeners to each square
+    //Return (board?)
+})
+
+    
+
+//game object - module
+
+    //Declare players
+    //Starting point
+    //Selectors
+    //Winning conditions (Array of arrays)
+    //Check winner
+    //Alert next player
+    //Next player
+    //Declare tie
+    //Return
